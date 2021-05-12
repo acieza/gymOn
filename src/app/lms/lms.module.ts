bca3a41afd4d0fb9e44bb4e2b9dafdb06d4c4525
acieza <asanjuan@arborfintech.com>
2021-05-12 22:19:57 +0200

    IMC

--- src/app/lms/lms.module.ts
+++ src/app/lms/lms.module.ts
@@ -6,10 +6,10 @@ import { LmsComponent } from './lms.component';
 
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { PerfilComponent } from './componentes/perfil/perfil.component';
-import { ImcComponent } from './componentes/imc/imc.component';
 import { MisClasesComponent } from './componentes/mis-clases/mis-clases/mis-clases.component';
 import { VideosComponent } from './componentes/videos/videos.component';
 import { AllClassComponent } from './componentes/all-class/all-class/all-class.component';
+import { ImcComponent } from './componentes/imc/imc.component';
 
 
 
