be50196d21c43150f2ef03cc58d7bc8f63e2d5f2
acieza <asanjuan@arborfintech.com>
2021-05-30 12:03:58 +0200

    Dani

--- src/app/lms/componentes/all-class/all-class/all-class.component.ts
+++ src/app/lms/componentes/all-class/all-class/all-class.component.ts
@@ -1,6 +1,8 @@
 import { Component} from '@angular/core';
+import { AuthService } from 'src/app/core/servicios/auth/auth.service';
 import { ClasesService } from 'src/app/core/servicios/clases/clases.service';
 import { Clase } from 'src/app/models/clase';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-all-class',
@@ -9,10 +11,17 @@ import { Clase } from 'src/app/models/clase';
 })
 export class AllClassComponent {
 
+  public usuario;
+
   clases:Clase[];
 
-  constructor(private clasesService: ClasesService) {
+  constructor(
+    private clasesService: ClasesService,
+    private authService: AuthService
+    ) {
     this.cargaClase();
+    this.usuario = authService.usuario;
+    console.log(this.usuario._id)
   }
 
   cargaClase(){
@@ -30,6 +39,30 @@ export class AllClassComponent {
     }else{
         return `http://localhost:3000/imgClase/${imagen}`;
     }
+}
+
+AddClase(idC:string, nombre:string){
+
+  this.authService.addClass(this.usuario._id,idC)
+        .subscribe( resp =>{
+          // this.cargaUsers()
+          console.log('*****Clase Añadida*****')
+          Swal.fire({
+            //position: 'top-end',
+            icon: 'success',
+            title: 'Clase Añadida con éxito',
+            showConfirmButton: true,
+            timer: 1500
+          });
+          // this.router.navigateByUrl(`/admin/Usuarios`)
+     
+        }, (err) => {
+         Swal.fire({
+           icon:'error',
+           title:'Ya estás apuntado a '+ nombre,
+           text: err.error.msg,         
+         })     
+        });
 }
 
 }
